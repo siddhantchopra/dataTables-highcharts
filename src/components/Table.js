2a058662adff92150666622225a61fd73f670c67
siddhantchopra <siddanthchopra@gmail.com>
2020-09-24 00:07:41 +0530

    changed code in table.js

--- src/components/Table.js
+++ src/components/Table.js
@@ -15,9 +15,8 @@ export const Table = (props) => {
     // }
     useEffect(()=>{
         $(document).ready(function () {
-            $('#example').DataTable({"paging":   false,  "info":     false});
+            $('#example').DataTable({"paging": false,"info": false});
         });  
-       
     },[])
     return (<table className="table" id="example">
             <thead>
@@ -34,7 +33,7 @@ export const Table = (props) => {
                 {
                     props.table.map((data, index) => <tr key={index}>
                         <td>{data.category}</td>
-                      { <td>{data.impactPercent}</td>}
+                        <td>{data.impactPercent}</td>
                         <td>{data.occurences}</td>
                         <td>{data.high}</td>
                         <td>{data.medium}</td>
@@ -46,3 +45,10 @@ export const Table = (props) => {
 
     )
 }
+
+{/* <th scope="col">RISK CATEGORY</th>
+                    <th scope="col">IMPACT<button data-sorttype="impactPercent" data-sort="desc"><i className="fa fa-angle-up"></i></button><button data-sorttype="impactPercent" data-sort="asc"><i className="fa fa-angle-down"></i></button></th>
+                    <th scope="col">OCCURENCES<button data-sorttype="occurences" data-sort="desc"><i className="fa fa-angle-up"></i></button><button data-sorttype="occurences" data-sort="asc"><i className="fa fa-angle-down"></i></button></th>
+                    <th scope="col">HIGH<button data-sorttype="high" data-sort="desc"><i className="fa fa-angle-up"></i></button><button  data-sorttype="high" data-sort="asc"><i className="fa fa-angle-down"></i></button></th>
+                    <th scope="col">MEDIUM<button button data-sorttype="medium" data-sort="desc"><i className="fa fa-angle-up"></i></button><button data-sorttype="medium" data-sort="asc"><i className="fa fa-angle-down"></i></button></th>
+                    <th scope="col">lOW<button data-sorttype="low" data-sort="desc"><i className="fa fa-angle-up"></i></button><button data-sorttype="low" data-sort="asc"><i className="fa fa-angle-down"></i></button></th> */}
